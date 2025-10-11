3cd6d081af2312394f12a93f5638aac1591db65c
PradhumnaPancholi <pnpancholi@protonmail.com>
2025-10-11 19:18:06 +0530

    erc20 checkpoint

--- src/ERC20.sol
+++ src/ERC20.sol
@@ -6,10 +6,10 @@ abstract contract ERC20 {
   // @dev Token Metadata
   string private _name;
   string private _symbol;
-  uint256 private _decimals;
+  uint8 private _decimals;
 
   // Handling Balance
-  uint256 private _tokenSupply;
+  uint256 private _totalSupply;
   mapping(address => uint256) private _balances;
   mapping(address => mapping(address => uint256)) private _allowances;
   //Events//
@@ -18,7 +18,7 @@ abstract contract ERC20 {
   /**
     * @dev Sets the values for name, symbol, and decimals
   **/
-  constructor(string memory name_, string memory symbol_, uint256 decimals_){
+  constructor(string memory name_, string memory symbol_, uint8 decimals_){
     _name = name_;
     _symbol = symbol_;
     _decimals = decimals_;
@@ -34,12 +34,12 @@ abstract contract ERC20 {
     return _symbol;
   }
 
-  function decimals() public view returns (uint256) {
+  function decimals() public view returns (uint8) {
     return _decimals;
   }
 
-  function tokenSupply() public view returns (uint256) {
-    return _tokenSupply();
+  function totalSupply() public view returns (uint256) {
+    return _totalSupply;
   }
 
   function allowance(address owner, address spender) public view returns (uint256) {
@@ -49,7 +49,6 @@ abstract contract ERC20 {
   function balanceOf(address account) public view returns (uint256) {
     return _balances[account];
   }
-
   // Public Functions
 
   function transfer(address to, uint256 amount) public returns (bool){
@@ -69,13 +68,54 @@ abstract contract ERC20 {
   }
 
   // Internal Functions//
+  function _approve(address owner, address spender, uint256 amount) internal virtual {
+    require(spender != address(0), "ERC-20: Approve To Zero Address");
+    require(owner != address(0), "ERC-20: Approve From Zero Address");
+    _allowances[owner][spender] = amount;
+    emit Approval(owner, spender, amount);
+  }
+
   function _transfer(address from, address to, uint256 amount) internal virtual {
-    require(from != address(0), "ERC-20: Transfer From The Zero Address");
-    require(to != address(0), "ERC-20: Transfer To The Zero Address");
-    require(_balances[from] >= amount, "ERC-20: Amount Exceeds Balance");
-    _balances[from] -= amount;
-    _balances[to] += amount;
+    require(from != address(0), "ERC-20: Transfer From Zero Address");
+    require(to != address(0), "ERC-20: Transfer To Zero Address");
+    uint256 accountBalance = _balances[from];
+    require(accountBalance >= amount, "ERC-20: Amount Exceeds Balance");
+    unchecked {
+      _balances[from] = accountBalance - amount;
+      _balances[to] += amount;
+    }
     emit Transfer(from, to, amount);
   }
 
+  function _mint(address account, uint256 amount) internal virtual {
+    require(account != address(0), "ERC-20: Mint To Zero Address");
+    _totalSupply += amount;
+    unchecked {
+      _balances[account] += amount;
+    }
+    emit Transfer(address(0), account, amount);
+  }
+
+  function _spendAllowance(address owner, address spender, uint256 amount) internal virtual{
+    uint256 currentAllowance = _allowances[owner][spender];
+
+    if (currentAllowance != type(uint256).max ) {
+      require(currentAllowance >= amount, "ERC-20: Insufficient Allowance");
+      unchecked {
+        _allowances[owner][spender] -= amount;
+      }
+    }
+  }
+ 
+  function _burn(address account, uint256 amount) internal virtual {
+    require(account != address(0), "ERC-20: Account Is An Zero Address");
+    uint256 accountBalance = _balances[account];
+    require(accountBalance >= amount, "ERC-20: Amount Exceeds Account Balance");
+    unchecked {
+      _balances[account] = accountBalance - amount;
+      _totalSupply -= amount;
+    }
+    emit Transfer(account, address(0), amount);
+  }
+  
 }
