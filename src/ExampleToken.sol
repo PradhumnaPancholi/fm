87b41e1b3daa9857cb2465c91cc35fc412c7fd03
Pradhumna Pancholi <pnpancholi07@gmail.com>
2025-10-12 11:36:19 +0530

    Added example token, mintable

---
+++ src/ExampleToken.sol
@@ -0,0 +1,22 @@
+//SPDX-License-Identifier: GPL.3.0
+pragma solidity ^0.8.0;
+
+import "./ERC20.sol";
+import "./Mintable.sol";
+
+contract MyToken is ERC20, Mintable{
+
+  constructor() ERC20("TestToken", "TST", 18){
+    // Mints 100 TST tokens for initial supply
+    _mint(msg.sender, 100 * 10**18);
+  }
+
+  //Mintinging another batch of token.
+  //Resolves mint override
+  function _mint(address to, uint256 amount) internal override(ERC20, Mintable) {
+    ERC20._mint(msg.sender, 100 * 10**18);
+  } 
+
+
+}
+
