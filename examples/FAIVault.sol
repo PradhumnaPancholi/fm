86df0851b5cbd9a05dec2df5cb00b1e9047ff7ff
Pradhumna Pancholi <pnpancholi07@gmail.com>
2025-11-08 12:24:33 +0530

    implemented FAI, and added deposit for FAIVault

--- examples/FAIVault.sol
+++ examples/FAIVault.sol
@@ -1,6 +1,7 @@
 //SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.20;
 
+
 contract MockPriceFeed {
   uint256 public price = 10000 * 10**18;
 
@@ -8,6 +9,8 @@ contract MockPriceFeed {
     price = newPrice;
   } 
 }
+
+import "@fm/IERC20.sol";
 /*
 * @author Pradhumna Pancholi
 * @title FAIVault
@@ -17,18 +20,44 @@ contract MockPriceFeed {
 * 3. Liquidate
 */
 contract FAIVault {
+
+  MockPriceFeed public priceFeed;
+  
+  IERC20 public immutable fai;
+
   struct Position {
     uint256 collateral;
     uint256 debt;
   }
 
+  event FAIMinted(address indexed to, uint256 indexed amount);
   uint256 public totalCollateral;
   uint256 public debt;
-  uint256 public constant immutable LIQUIDATION_RATIO = 150 * 10**16;
-  uint256 public constant immutable LIQUIDATION_PENALTY = 10 * 10**16;
+  uint256 public constant LIQUIDATION_RATIO = 1.5e18;
+  uint256 public constant LIQUIDATION_PENALTY = 1e16;
+  
   mapping(address => Position) positions;
 
-  function depositCollateralAndMint() public{}
+  constructor(address faiAddress) {
+    fai = IERC20(faiAddress);
+    priceFeed = new MockPriceFeed();
+  }
+
+  function depositCollateralAndMint(uint256 amount) payable public returns (bool){
+    require(amount > 0, "FAIVault: can not mint zero tokens");
+    require(msg.value > 0, "FAIVault: can not mint with zero ETH");
+    uint256 providedCollateral = (msg.value / 1e18) * priceFeed.price();
+    uint256 minCollateralRequired = amount * LIQUIDATION_RATIO / 1e18;
+    require(providedCollateral >= minCollateralRequired, "FAIVault: not enough collateral");
+    //update state
+    positions[msg.sender].collateral += msg.value;
+    positions[msg.sender].debt += amount; 
+    totalCollateral += msg.value;
+    debt += amount;
+
+    fai.mint(msg.sender, amount);
+    emit FAIMinted(msg.sender, amount);
+  }
   function burnAndWithdrawCollateral() public {}
   function liquidate() public{}
 }
